38fa0f9ad151e1850507dbaba278317f64144abd
di-photos <di-photos@live.cn>
2024-10-10 16:04:56 +0800

    update config

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Nasu",
+  base: '/vitepress/',
   description: "Nasu Website",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
